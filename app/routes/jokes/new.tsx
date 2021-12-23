099f81a7336f8c11ee9d4ba0416814569c48fa6b
roblesdotdev <robles.contactme@gmail.com>
2021-12-23 08:45:42 -0300

    adding actions

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,3 +1,19 @@
+import { ActionFunction, redirect } from "remix";
+import { db } from "~/utils/db.server";
+
+export const action: ActionFunction = async ({ request }) => {
+  let form = await request.formData();
+  let name = form.get("name");
+  let content = form.get("content");
+  if (typeof name !== "string" || typeof content != "string") {
+    throw new Error("Form submitted incorrectly");
+  }
+  let joke = await db.joke.create({
+    data: { name, content },
+  });
+  return redirect(`/jokes/${joke.id}`);
+};
+
 export default function NewJokeRoute() {
   return (
     <div>
